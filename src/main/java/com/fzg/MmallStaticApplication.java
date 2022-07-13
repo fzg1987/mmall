2a2d735a13ff02fcf8143285544ea104842695c7
fzg1987 <beginer@yeah.net>
2022-07-13 20:12:40 +0800

    Initial commit

---
+++ src/main/java/com/fzg/MmallStaticApplication.java
@@ -0,0 +1,11 @@
+package com.fzg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmallStaticApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MmallStaticApplication.class,args);
+    }
+}
